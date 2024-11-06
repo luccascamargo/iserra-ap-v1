15277583490703d81e004284c8a50b727341a8f6
Lucas Camargo <lucascamargo.dev@gmail.com>
2024-11-06 16:37:37 -0300

    Enviando projeto

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace iserra_api.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase {
+
+        // [HttpGet(Name = "GetWeatherForecast")]
+    }
+}
